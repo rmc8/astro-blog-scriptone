00b40565eb3d1c2f131b121b298a0adc492f9bf3
rmc-8.com <kmprmc@gmail.com>
2025-06-03 10:26:43 +0900

    feat: Add updated date and table of contents to blog posts
    This commit introduces the following changes to blog posts:
    
    - **Updated Date:**
        - Adds an `updatedDate` field to the blog post frontmatter, allowing authors to specify a last updated timestamp.
        - Displays the `updatedDate` next to the `PostDate` in the article header, providing readers with information about content freshness.
        - Implements logic to only display the `updatedDate` if it differs from the `PostDate`.
    - **Table of Contents (ToC):**
        - Adds a `toc` field to the blog post frontmatter, enabling authors to control the display of the table of contents.
        - Integrates a new `ToC` component to dynamically generate and display a table of contents based on the article's headings.
        - The ToC is conditionally rendered based on the `toc` frontmatter field.
    - **Frontmatter Configuration:**
        - Updates `frontmatter.json` to include `updatedDate`, `slug`, and `toc` fields for blog posts.
        - Changes the default file type for blog posts to `mdx`.
    - **Styling and Structure:**
        - Refines the layout of the article header to accommodate the new `updatedDate`.
        - Adjusts global styles for better readability and responsiveness, particularly for headings and links within the article content.
        - Improves the structure of the `Article.astro` component for better maintainability.
    - **New Blog Post:**
        - Adds a new Japanese blog post example (`2025-06-03-rust-raspberry-pi-learning-kit-table-lamp.mdx`) to demonstrate the new features.
    
    These changes enhance the user experience by providing more context about content freshness and improving navigation within longer articles.

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,11 +3,14 @@ import { z, defineCollection } from "astro:content";
 const blogSchema = z.object({
     title: z.string(),
     description: z.string().optional(),
+    slug: z.string().optional(),
     date: z.date(),
+    updatedDate: z.date().optional(),
     preview: z.string().optional(),
     draft: z.boolean().default(false),
     tags: z.array(z.string()).optional(),
     categories: z.array(z.string()).optional(),
+    toc: z.boolean().default(true),
 });
 const blog_ja = defineCollection({
     type: "content",
